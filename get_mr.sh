0a896a15cd63303a8c571306b895a0c07d12a02c
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2014-01-20 14:02:11 +0100

    Switched from submodules to mr

---
+++ get_mr.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+git clone git://myrepos.branchable.com/ mr
+ln -s -f -v $(pwd)/mr/mr ~/usr/bin/mr
+
---
+++ register_repos.sh
@@ -0,0 +1,18 @@
+
+register_repo()
+{
+URL=$1
+FOLDER=$2
+git clone $URL $FOLDER
+mr register $FOLDER
+mr config $FOLDER update="git stash && git pull --rebase && git stash pop"
+}
+
+touch .mrconfig
+
+TRUSTFILE=~/.mrtrust
+echo "$(pwd)/.mrconfig" >> $TRUSTFILE
+
+register_repo git@github.com:obreitwi/dot_vim.git vim
+register_repo git@github.com:obreitwi/dot_zsh.git zsh
+
