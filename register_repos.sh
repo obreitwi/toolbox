f4effb6a6a2b602ba4cbea4d3dcb3b3e59728df6
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2014-01-31 03:43:38 +0100

    Added tmux config

--- register_repos.sh
+++ register_repos.sh
@@ -13,6 +13,9 @@ touch .mrconfig
 TRUSTFILE=~/.mrtrust
 echo "$(pwd)/.mrconfig" >> $TRUSTFILE
 
+register_repo git@github.com:obreitwi/pydemx.git pydemx
 register_repo git@github.com:obreitwi/dot_vim.git vim
 register_repo git@github.com:obreitwi/dot_zsh.git zsh
+register_repo git@github.com:obreitwi/dot_tmux.git tmux
+
 
