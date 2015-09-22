5d412c1afca5f909cdbf9d7eb99e1f6187edea67
Oliver Breitwieser <obreitwi@pio2pc145.pio.uni-heidelberg.de>
2015-09-22 16:28:21 +0200

    Updated mr install script

--- get_mr.sh
+++ get_mr.sh
@@ -1,5 +1,13 @@
 #!/bin/zsh
 
-git clone https://github.com/joeyh/myrepos.git mr
-ln -s -f -v $(pwd)/mr/mr ~/usr/bin/mr
+PATH_DIR=$HOME/.local/bin
 
+git clone https://github.com/joeyh/myrepos.git mr
+mkdir -p ~/.local/bin
+ln -s -f -v $(pwd)/mr/mr $PATH_DIR/mr
+
+echo $PATH | grep "$PATH_DIR" > /dev/null
+
+if [ $? -eq 1 ]; then
+	export PATH=$PATH:$PATH_DIR
+fi
