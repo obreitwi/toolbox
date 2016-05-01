d2a4184f442d70ab6c01d749dace9e50380a56e7
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2016-05-01 13:53:58 +0200

    rpush: Improved filename handling

--- small-tools/rpush/rpush.py
+++ small-tools/rpush/rpush.py
@@ -2,14 +2,15 @@
 # encoding: utf-8
 """
 Usage:
-    rpush [--config <cfg> ] list
-    rpush [--config <cfg> ] clean (all|<num> ...)
-    rpush [--config <cfg> ] [--push] (<file> | --alias <file_in> <file_out>) ...
-    rpush -h | --help
+    rpush [-v] [--config <cfg>] list
+    rpush [-v] [--config <cfg>] clean (all|<num> ...)
+    rpush [-v] [--config <cfg>] [--push] (<file> | --alias <file_in> <file_out>) ...
+    rpush [-v] --help
 
 Options:
     -h --help       Show this screen.
     --version       Show version.
+    -v --verbose    Enable verbose output
 
     --push          Force pushing of whatever follows
 
@@ -27,26 +28,32 @@ Options:
 
 from __future__ import print_function
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 import os
+import os.path as osp
 import sys
 import subprocess as sp
 
 import random
 import string
 
+import shlex
+
 import itertools
 
 import logging
 logging.basicConfig(
-        format='%(asctime)s | %(funcName)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S',
+        #  format='%(asctime)s | %(funcName)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S',
+        format='%(asctime)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S',
         level = logging.INFO)
 
 if sys.version_info < (3, 0):
     import ConfigParser as configparser
+    import urllib
 else:
     import configparser
+    import urllib.parse as urllib
 
 # much inspiration to be taken from:
 # http://code.activestate.com/recipes/576810-copy-files-over-ssh-using-paramiko/
@@ -86,8 +93,7 @@ class RPushHandler(object):
         else:
             self.cmd_push()
 
-
-    def extra_handling( self ):
+    def extra_handling(self):
         if not self.args["--push"] and "clean" in self.args[ "<file>" ]:
             print("Please specify all or the filenumber you want to clean.")
             print("If you want to push file \"clean\" please specify push.")
@@ -97,21 +103,21 @@ class RPushHandler(object):
         for f_in, f_out in itertools.chain(zip(self.args[ "<file>" ], self.args[ "<file>" ]),
                                 zip(self.args[ "<file_in>" ], self.args[ "<file_out>" ])):
             rfolder = random_string()
-            self.run_ssh_command("mkdir "+ rfolder)
-            path = "{0}/{1}".format(rfolder, os.path.basename( f_out ))
+            self.run_ssh_command("mkdir "+ shlex.quote(rfolder))
+            path = osp.join(rfolder, os.path.basename(f_out))
             self.run_scp_command(f_in, path)
-            self.run_ssh_command("chown :{1} {0} && chmod g+r {0}".format( path, self.www_group ))
+            self.run_ssh_command("chown :{1} {0} && chmod g+r {0}".format(
+                shlex.quote(path), self.www_group))
 
-            print( "{0}/{1}".format(self.url, path))
+            print(self.encode_url(path))
+            #  print( "{0}/{1}".format(self.url, path))
 
-
-    def cmd_list( self ):
+    def cmd_list(self):
         # for i,f in enumerate( self.list_complete_directory()):
         for i,f in enumerate(self.get_complete_remote_content()):
             print("[{0}]\t{1}\t{2}/{1}".format(i, f, self.url))
 
-
-    def cmd_clean( self ):
+    def cmd_clean(self):
         complete_content = self.get_complete_remote_content()
 
         if not self.args[ "all" ]:
@@ -122,42 +128,36 @@ class RPushHandler(object):
         for path in to_delete:
             self.remove_remote(path)
 
-
     def remove_remote(self, path):
-        folder, file = path.split("/")
+        folder, file = osp.split(path)
 
-        self.run_ssh_command("rm " + path)
-        self.run_ssh_command("rmdir " + folder)
-
+        self.run_ssh_command("rm " + shlex.quote(path))
+        self.run_ssh_command("rmdir " + shlex.quote(folder))
 
     def run_ssh_command(self, command):
         logging.debug(command)
-        proc = sp.Popen(self.ssh_args + ["cd {0} && ".format( self.basefolder ) + command],
-                stdout = sp.PIPE, stderr = sp.PIPE)
-        rc = proc.wait()
-        contents, warnings = proc.communicate()
-
-        if rc != 0:
-            logging.warn(warnings)
-
-        return contents
+        return self.ssh_exec(self.ssh_args
+            + ["cd {0} && {1}".format(shlex.quote(self.basefolder), command)])
 
     def run_scp_command(self, path_from, path_to):
         logging.debug( "SCP: {0} -> {1}".format(path_from, path_to))
-        proc = sp.Popen(["scp", path_from, "{0}:{1}/{2}".format(self.host, self.basefolder, path_to)],
-                stdout = sp.PIPE, stderr = sp.PIPE)
+        return self.ssh_exec(["scp", path_from, "{0}:'{1}/{2}'".format(
+            self.host, self.basefolder, path_to)])
+
+    def ssh_exec(self, cmd):
+        logging.debug(" ".join(cmd))
+        proc = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
         rc = proc.wait()
         contents, warnings = proc.communicate()
-
         if rc != 0:
             logging.warn(warnings)
-
         return contents
 
+    def encode_url(self, path):
+        return osp.join(self.url, urllib.quote(path))
 
     def get_complete_remote_content(self):
         return self.list_directory("*/*")
-
 
     def list_directory(self, path):
         contents = self.run_ssh_command("ls -rt {0}".format(path))
@@ -173,7 +173,9 @@ class RPushHandler(object):
 if __name__ == "__main__":
     random.seed()
     arguments = docopt(__doc__, version=__version__)
-    # print(arguments)
+    if arguments["--verbose"]:
+        logging.root.setLevel(logging.DEBUG)
+        #  logging.debug(arguments)
     RPushHandler(arguments)
 
 
